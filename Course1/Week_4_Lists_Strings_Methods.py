0bfd3664d7d7a839556a466ee240f3323247cb3e
SuryaTall <99516879+SuryaTall@users.noreply.github.com>
2023-10-12 17:24:58 +0000

    main

--- Course1/Week_4_Lists_Strings_Methods.py
+++ Course1/Week_4_Lists_Strings_Methods.py
@@ -7,6 +7,8 @@ trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathma
 for i in trav_dest:
     if i == 'London':
         trav_dest.remove(i)
+    else:
+        continue
 #This shows an error on Github, but works in the Runestone Environment
 
 #Write code to add ‘Guadalajara’ to the end of the list trav_dest using a list method
