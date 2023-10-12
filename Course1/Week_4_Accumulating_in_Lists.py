063e7d8b553aec6873905e825577fe34d0126495
SuryaTall <99516879+SuryaTall@users.noreply.github.com>
2023-10-12 17:16:30 +0000

    main

---
+++ Course1/Week_4_Accumulating_in_Lists.py
@@ -0,0 +1,10 @@
+#Currently there is a string called str1.
+# Write code to create a list called chars which should contain the characters from str1.
+# Each character in str1 should be its own element in the list chars.
+
+str1 = "I love python"
+a = 0
+chars = []
+for i in str1:
+    chars.append(i)
+
